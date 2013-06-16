4142a7b9ddbebe98095fbb5267fceb236d2f075f
Caleb Abraham <cabraham@Macintosh-4.local>
2013-06-16 16:36:44 -0400

    Initial Commit

---
+++ CrystalBallCJA/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CrystalBallCJA
+//
+//  Created by Caleb Abraham on 6/16/13.
+//  Copyright (c) 2013 Caleb Abraham. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
